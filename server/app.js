4f525f40ce1b8d58ec33ab1d87774e5aec9df057
joshjk <jokrienke@comcast.net>
2023-04-29 15:32:03 -0700

    completed

--- server/app.js
+++ server/app.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const app = express();
+const dog = require('./routes/dogs')
+app.use(express.json())
+require(dotenv).configure()
 
+app.use('/static', express.static('assets'));
+
+// require('express-asnyc-errors')
 // For testing purposes, GET /
+
+const logger = (req, res, next) => {
+
+  console.log(`${req.method} ${req.url}`)
+  res.on('finish', () => {
+    console.log(`Status: ${res.statusCode}`)
+  });
+  next()
+}
+
+app.use(logger)
+
 app.get('/', (req, res) => {
   res.json("Express server running. No content provided at root level. Please use another route.");
 });
-
 // For testing express.json middleware
 app.post('/test-json', (req, res, next) => {
   // send the body as JSON with a Content-Type header of "application/json"
@@ -18,6 +35,25 @@ app.post('/test-json', (req, res, next) => {
 app.get('/test-error', async (req, res) => {
   throw new Error("Hello World!")
 });
+app.use("/dogs", dog)
+
+app.use((req, res, next) => {
+  const err = new Error(`Error, requested resource could not be found`)
+  err.statusCode = 404
+  throw err;
+})
+
+app.use((err, req, res, next) => {
+
+  const response = {
+    statusCode: err.statusCode || 500,
+    message: err.message || "something went wrong",
+    stack: process.env.NODE_ENV ==="production" ? undefined : err.stack
+  }
+  console.log(err);
+  res.status(err.statusCode)
+  res.json(response)
+})
 
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
--- server/routes/dogs.js
+++ server/routes/dogs.js
@@ -1,4 +1,4 @@
-// ------------------------------  SERVER DATA ------------------------------  
+// ------------------------------  SERVER DATA ------------------------------
 
 let nextDogId = 1;
 function getNewDogId() {
@@ -18,7 +18,7 @@ const dogs = [
   }
 ];
 
-// ------------------------------  MIDDLEWARES ------------------------------ 
+// ------------------------------  MIDDLEWARES ------------------------------
 
 const validateDogInfo = (req, res, next) => {
   if (!req.body || !req.body.name) {
@@ -40,7 +40,7 @@ const validateDogId = (req, res, next) => {
   next();
 }
 
-// ------------------------------  ROUTE HANDLERS ------------------------------  
+// ------------------------------  ROUTE HANDLERS ------------------------------
 
 // GET /dogs
 const getAllDogs = (req, res) => {
@@ -82,6 +82,28 @@ const deleteDog = (req, res) => {
   res.json({ message: "success" });
 };
 
-// ------------------------------  ROUTER ------------------------------  
+// const { get } = require('cypress/types/jquery');
+// ------------------------------  ROUTER ------------------------------
 
-// Your code here
+
+
+const express = require('express')
+
+
+let router = express.Router();
+
+// router.use(validateDogInfo)
+// router.use(validateDogId)
+
+router.get("/", getAllDogs)
+
+router.get("/:dogId", validateDogId, getDogById)
+
+router.post('/', validateDogInfo, createDog)
+
+router.put('/:dogId', validateDogId, updateDog)
+
+router.delete('/:dogId' ,validateDogId, deleteDog)
+router.use("/:dogid/foods",  validateDogId, require('./dog-foods'))
+
+module.exports = router;
